cb8b3d85983d19668755fe1724baee77e51d82ef
DJTheBest <djthebest53@gmail.com>
2026-09-05 17:21:01 -0500

    Fix AitherBackend CORS for Aither app account requests

--- app/main.py
+++ app/main.py
@@ -38,7 +38,9 @@ app.add_middleware(
     allow_origins=settings.cors_origin_list,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
+    allow_headers=["Accept", "Content-Type", "Authorization"],
+    expose_headers=["Content-Type"],
+    max_age=86400,
 )
 
 app.include_router(status_router)
